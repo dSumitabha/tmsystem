62af9f39c45f62ddcc1ed2a9213fc172af1ada8a
sumitabha4 <sumitabha4@gmail.com>
2025-11-01 13:26:55 +0000

    basic app kayout setup

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function(){
+    return(
+        <footer>
+            <small>Developed with ❤️ by Sumitabha</small>
+        </footer>
+    )
+}
---
+++ src/app/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <header>
+            <h1 className="text-white">Task Management System</h1>
+        </header>
+
+    )
+}
