198901ca7ab720ddb43be6b0398af208f6b22fde
sumitabha4 <sumitabha4@gmail.com>
2025-11-02 12:30:00 +0000

    header footer sticky design

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function(){
     return(
-        <footer>
-            <small>Developed with ❤️ by Sumitabha</small>
+        <footer className="sticky bottom-0 py-4 bg-slate-950 text-center">
+            <small className="text-center">Developed with ❤️ by Sumitabha</small>
         </footer>
     )
 }
--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header(){
     return(
-        <header>
-            <h1 className="text-white">Task Management System</h1>
+        <header className="sticky top-0 py-4 bg-slate-950">
+            <h1 className="text-white text-2xl font-bold pl-4">Task Management System</h1>
         </header>
 
     )
