7d4303d307e556bb3627a8c1db75dae830d12889
sumitabha4 <sumitabha4@gmail.com>
2025-11-04 19:14:17 +0000

    installed combo box and added sonner at layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { Toaster } from 'sonner';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
         <Header />
         {children}
         <Footer />
+        <Toaster position="top-center" />
       </body>
     </html>
   );
