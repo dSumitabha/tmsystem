90a6715c0cc788cfa81459b2b09bee5c26b7a3d1
sumitabha4 <sumitabha4@gmail.com>
2025-11-08 20:52:07 +0000

    create-task authorization

--- src/app/create-task/page.js
+++ src/app/create-task/page.js
@@ -1,7 +1,17 @@
 import TaskForm from "../components/TaskForm";
+import { getUserFromToken } from "@/lib/getUserFromToken";
+import { redirect } from 'next/navigation';
 
-export default function (){
-    return(
+export default async function TaskPage() {
+    const user = await getUserFromToken();
+
+    // if regular user try to access it 
+    if (!user?.isAdmin) {
+        return redirect('/');
+    }
+
+    // If the user is an admin, render the TaskForm component
+    return (
         <TaskForm />
-    )
+    );
 }
