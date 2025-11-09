4ee8aeb63af210ff0cfb3cfbe445cd407f391943
sumitabha4 <sumitabha4@gmail.com>
2025-11-09 09:21:12 +0000

    user seeder updated

--- src/app/api/demonstrate/seed-users/route.js
+++ src/app/api/demonstrate/seed-users/route.js
@@ -2,39 +2,21 @@ import { NextResponse } from 'next/server';
 import bcrypt from 'bcryptjs';
 import User from '@/models/User';
 import dbConnect from '@/lib/db';
+import path from 'path';
+import fs from 'fs';
 
 export async function GET(request) {
   try {
     await dbConnect();
 
+    const usersFilePath = path.resolve('src/data', 'users.json');
+    const usersData = JSON.parse(fs.readFileSync(usersFilePath, 'utf8'));
     // Sameple user data to make demonstration easy
-    const usersData = [
-      { fullName: 'Surya Kumar Yadav', email: 'surya@gmail.com', password: 'password123' },
-      { fullName: 'Rohit Sharma', email: 'rohit@gmail.com', password: 'password123' },
-      { fullName: 'Yusuf Pathan', email: 'yusuf@gmail.com', password: 'password123' },
-      { fullName: 'Irfan Pathan', email: 'irfan@gmail.com', password: 'password123' },
-      { fullName: 'Robin Uthappa', email: 'robin@gmail.com', password: 'password123' },
-      { fullName: 'Murali Vijay', email: 'murali@gmail.com', password: 'password123' },
-      { fullName: 'Ravichandran Ashwin', email: 'ravichandran@gmail.com', password: 'password123' },
-      { fullName: 'Ajinkya Rahane', email: 'ajinkya@gmail.com', password: 'password123' },
-      { fullName: 'Shikhar Dhawan', email: 'shikhar@gmail.com', password: 'password123' },
-      { fullName: 'Bhuvneshwar Kumar', email: 'bhuvneshwar@gmail.com', password: 'password123' },
-
-      { fullName: 'Brad Hodge', email: 'brad@gmail.com', password: 'password123' },
-      { fullName: 'Matthew Hayden', email: 'matthew@gmail.com', password: 'password123' },
-      { fullName: 'Michael Clarke', email: 'michael@gmail.com', password: 'password123' },
-      { fullName: 'Shane Watson', email: 'shane@gmail.com', password: 'password123' },
-      { fullName: 'Mitchell Johnson', email: 'mitchell@gmail.com', password: 'password123' },
-      { fullName: 'Nathan Hauritz', email: 'nathan@gmail.com', password: 'password123' },
-      { fullName: 'David Hussey', email: 'david@gmail.com', password: 'password123' },
-      { fullName: 'George Bailey', email: 'george@gmail.com', password: 'password123' },
-      { fullName: 'Aaron Finch', email: 'aaron@gmail.com', password: 'password123' },
-      { fullName: 'James Pattinson', email: 'james@gmail.com', password: 'password123' }
-    ];
+    
 
     // Check if any users already exist
     for (let i = 0; i < usersData.length; i++) {
-      const { fullName, email, password } = usersData[i];
+      const { fullName, email, password, isAdmin } = usersData[i];
 
       const existingUser = await User.findOne({ email });
 
@@ -50,7 +32,7 @@ export async function GET(request) {
         fullName,
         email,
         password: hashedPassword,
-        isAdmin: false
+        isAdmin: isAdmin === 'true' ? true : false
       });
 
       await newUser.save();
